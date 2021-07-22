9d0cad15e854ef0702e4e552b4fdac6972fc3837
navyd <dhjnavyd@gmail.com>
2021-07-23 00:51:03 +0800

    optimize the output of replace command to folder and support file overwrite

--- src/cmd.rs
+++ src/cmd.rs
@@ -1,15 +1,17 @@
 use crate::image::{Checker, Link, LinkType};
 use crate::CRATE_NAME;
-use anyhow::{anyhow, Error, Result};
+use anyhow::{anyhow, bail, Error, Result};
 use futures::future;
-use log::{error, info, warn};
+use log::{debug, info, trace, warn};
 use std::collections::HashSet;
+use std::fmt::Debug;
 use std::fs;
 use std::fs::{create_dir, create_dir_all};
 use std::io::stdout;
 use std::path::{Path, PathBuf};
 use std::time::Duration;
 use structopt::StructOpt;
+use tokio::fs::write;
 
 #[derive(StructOpt, Debug)]
 enum Opt {
@@ -17,15 +19,15 @@ enum Opt {
         #[structopt(flatten)]
         common: Common,
         #[structopt(flatten)]
-        args: CheckerArgs,
+        checker_args: CheckerArgs,
     },
     Replace {
         #[structopt(flatten)]
         common: Common,
         #[structopt(flatten)]
-        args: CheckerArgs,
-        #[structopt(short, long, parse(from_os_str))]
-        output: PathBuf,
+        checker_args: CheckerArgs,
+        #[structopt(flatten)]
+        replace_args: ReplaceArgs,
     },
 
     Completion,
@@ -34,12 +36,8 @@ enum Opt {
 impl Opt {
     fn common(&self) -> Option<&Common> {
         match self {
-            Opt::Check { common, args: _ } => Some(common),
-            Opt::Replace {
-                common,
-                args: _,
-                output: _,
-            } => Some(common),
+            Opt::Check { common, .. } => Some(common),
+            Opt::Replace { common, .. } => Some(common),
             Opt::Completion => None,
         }
     }
@@ -49,7 +47,7 @@ impl Opt {
 pub struct CheckerArgs {
     #[structopt(long, parse(try_from_str = parse_duration_secs), default_value = "5")]
     pub connect_timeout: Duration,
-    #[structopt(long, parse(try_from_str), default_value = "local")]
+    #[structopt(long, parse(try_from_str), default_value = "all")]
     pub image_link_type: LinkType,
 }
 
@@ -59,6 +57,14 @@ fn parse_duration_secs(s: &str) -> Result<Duration> {
         .map_err(Into::into)
 }
 
+#[derive(Clone, Debug, StructOpt)]
+struct ReplaceArgs {
+    #[structopt(short = "O", long, parse(from_os_str))]
+    output: PathBuf,
+    #[structopt(short = "o", long)]
+    overwrite: bool,
+}
+
 #[derive(StructOpt, Debug, Default, Clone)]
 struct Common {
     /// log level. default error
@@ -66,8 +72,8 @@ struct Common {
     verbose: u8,
 
     /// path of input files
-    #[structopt(parse(from_os_str))]
-    paths: Vec<PathBuf>,
+    #[structopt(parse(from_os_str), default_value = ".")]
+    input: PathBuf,
 
     /// use `,` limit the markdown docs. like `--extensions md,markdown,mk`.
     /// default md
@@ -89,20 +95,25 @@ pub async fn run() -> Result<()> {
     if let Some(common) = opt.common() {
         init_log(common.verbose)?;
     }
-
+    trace!("opt args: {:?}", opt);
     match opt {
-        Opt::Check { common, args } => {
+        Opt::Check {
+            common,
+            checker_args: args,
+        } => {
             let paths = common.load_input_paths()?;
             let checker = Checker::new(args);
             do_check(&paths, &checker).await;
         }
         Opt::Replace {
-            args,
-            output,
+            checker_args,
             common,
+            replace_args,
         } => {
-            let paths = common.load_input_paths()?;
-            do_replace(&paths, &output, args).await
+            if common.input == replace_args.output && common.input.is_dir() {
+                bail!("prohibit output to input folder: {:?}", common.input);
+            }
+            do_replace(common, checker_args, replace_args).await?;
         }
         Opt::Completion => {
             use structopt::clap::Shell;
@@ -113,74 +124,164 @@ pub async fn run() -> Result<()> {
     Ok(())
 }
 
-async fn do_replace(inputs: &[PathBuf], output: &Path, args: CheckerArgs) {
-    if matches!(args.image_link_type, LinkType::None) {
-        println!("stopping replace action with args: image_link_type=None");
-        return;
+fn check_output_path(output: impl AsRef<Path>, overwrite: bool) -> Result<()> {
+    let output = output.as_ref();
+    let output_str = output
+        .to_str()
+        .ok_or_else(|| anyhow!("{:?} to str error", output))?;
+    debug!("checking if output {} is available", output_str);
+    if !output.exists() {
+        info!("creating output dir: {}", output_str);
+        create_dir(&output).map_err::<Error, _>(Into::into)?;
+    } else if !overwrite && fs::read_dir(&output).map_or(true, |mut dir| dir.next().is_some()) {
+        bail!("non empty output dir: {}", output_str);
     }
-    if output.exists() {
-        if fs::read_dir(&output).map_or(true, |mut dir| dir.next().is_some()) {
-            eprintln!("non empty out dir: {:?}", output);
-            return;
-        }
-    } else {
-        info!("creating out dir: {:?}", output);
-        create_dir(&output).unwrap_or_else(|e| panic!("create dir {:?} error: {}", output, e));
+    Ok(())
+}
+
+async fn write_output(output: impl AsRef<Path>, text: &str, overwrite: bool) -> Result<()> {
+    let output = output.as_ref();
+    debug!(
+        "writing replacement result to path {:?}, overwrite: {}",
+        output, overwrite
+    );
+    let parent_dir = output
+        .parent()
+        .ok_or_else(|| anyhow!("no parent in path: {:?}", output))?;
+    if output.is_file() && !overwrite {
+        bail!(
+            "overwriting args:[--overwrite] of existing file {:?} is not allowed",
+            output
+        )
     }
-    let output = output.canonicalize().unwrap();
+    if !parent_dir.exists() {
+        info!("creating all parent dir for new output path: {:?}", output);
+        create_dir_all(parent_dir).map_err::<Error, _>(Into::into)?;
+    }
+    write(&output, text).await.map_err::<Error, _>(Into::into)
+}
 
-    let checker = Checker::new(args);
-    let jobs = inputs
-        .iter()
-        .map(|path| tokio::spawn(embed_task(path.clone(), output.clone(), checker.clone())))
-        .collect::<Vec<_>>();
+async fn do_replace(common: Common, ch_args: CheckerArgs, re_args: ReplaceArgs) -> Result<()> {
+    if matches!(ch_args.image_link_type, LinkType::None) {
+        warn!("stopping replace action with args: image_link_type=None");
+        return Ok(());
+    }
 
-    for job in future::join_all(jobs).await {
-        if let Err(e) = job.map_err(Into::into).and_then(|e| e) {
+    let paths = common.load_input_paths()?;
+    check_output_path(&re_args.output, re_args.overwrite)?;
+    // guaranteed to be an absolute path
+    let input = common.input.canonicalize()?;
+    let checker = Checker::new(ch_args);
+
+    let mut handles = vec![];
+    for path in paths {
+        let path = path.clone();
+        let input = input.clone();
+        let checker = checker.clone();
+        let sub_path = get_sub_path(input, path.to_path_buf())?;
+        // cant use join for absolute path
+        debug_assert!(
+            sub_path.is_relative(),
+            "Forbidden absolute path {:?}",
+            sub_path
+        );
+        let output = re_args.output.join(sub_path);
+        let overwrite = re_args.overwrite;
+        let handle = tokio::spawn(async move {
+            debug!("starting a replace task to {:?} from {:?}", output, path);
+            let (text, errs) = checker.embed_base64(&path).await?;
+            write_output(output, &text, overwrite).await?;
+            debug!(
+                "embed_base64 task completed, {} errors found for {:?}",
+                errs.len(),
+                path
+            );
+            // report errors
+            if !errs.is_empty() {
+                let mut msg = "".to_string();
+                if !errs.is_empty() {
+                    msg += &format!("found {} errors in {:?}\n", errs.len(), path);
+                    msg = errs.iter().fold(msg, |mut acc, e| {
+                        acc += &format!(
+                            "link: {}, cause: {}\n",
+                            e.downcast_ref::<Link>().unwrap(),
+                            e.root_cause()
+                        );
+                        acc
+                    });
+                }
+                eprintln!("{}", msg);
+            }
+            Ok::<_, Error>(())
+        });
+        handles.push(handle);
+    }
+    let size = handles.len();
+    println!("waiting {} tasks in {:?}", size, input);
+    for handle in future::join_all(handles).await {
+        if let Err(e) = handle.map_err(Into::into).and_then(|e| e) {
             eprintln!("async task failed: {}", e);
         }
     }
-    println!("all replace tasks completed");
+    println!("{} replace tasks completed", size);
+    Ok(())
 }
 
-async fn embed_task(path: PathBuf, output: PathBuf, checker: Checker) -> Result<()> {
-    // join check. temporary solution
-    if path.is_absolute() {
-        return Err(anyhow!(
-            "unsupported absolutely path `{:?}` with output: {:?}",
-            path,
-            output
-        ));
-    }
-    let output = output.join(&path);
-    let parent = output
-        .parent()
-        .ok_or_else(|| anyhow!("no parent in path: {:?}", output))?;
-    if !parent.exists() {
-        info!("creating all parent dir for {:?}", output);
-        create_dir_all(parent).map_err::<Error, _>(Into::into)?;
+/// 返回path相对base多出的路径。如base: /a/b, path: /a/b/c/d/e.md => sub path: c/d/e.md
+///
+///
+/// # Errors
+///
+/// - 任何相对路径
+/// - 相同的path==input且是文件
+fn get_sub_path<T: AsRef<Path> + Debug>(base: T, path: T) -> Result<PathBuf> {
+    let base = base.as_ref();
+    let path = path.as_ref();
+    trace!(
+        "generating new sub path with input: {:?}, path: {:?}",
+        base,
+        path
+    );
+    if base.is_relative() || path.is_relative() {
+        bail!(
+            "forbiden relative path for input: {:?}, path: {:?}",
+            base,
+            path
+        );
     }
 
-    let (text, errs) = checker.embed_base64(&path).await?;
-    let mut msg = "".to_string();
-    if !errs.is_empty() {
-        msg += &format!("found {} errors in {:?}\n", errs.len(), path);
-        msg = errs.iter().fold(msg, |mut acc, e| {
-            acc += &format!(
-                "link: {}, cause: {}\n",
-                e.downcast_ref::<Link>().unwrap(),
-                e.root_cause()
-            );
-            acc
-        });
+    if base == path {
+        trace!("same path {:?} only for input is file", base);
+        if base.is_file() {
+            bail!("same input path {:?} is file", base);
+        }
+        let name = path
+            .file_name()
+            .and_then(|s| s.to_str())
+            .ok_or_else(|| anyhow!("to str "))?
+            .parse::<PathBuf>()?;
+        return Ok(name);
     }
-    info!(
-        "writing replacement result to path {:?} from {:?}",
-        output, path
-    );
-    fs::write(&output, text).map_err::<Error, _>(Into::into)?;
-    println!("{}", msg);
-    Ok(())
+    let mut stack = vec![];
+    for p in path.ancestors() {
+        if p == base {
+            break;
+        }
+        let name = p
+            .file_name()
+            .and_then(|s| s.to_str())
+            .ok_or_else(|| anyhow!("to str error"))?;
+        stack.push(name);
+    }
+    if stack.is_empty() {
+        bail!("input {:?} is not an ancestor of path {:?}", base, path);
+    }
+    let mut new = PathBuf::new();
+    while let Some(v) = stack.pop() {
+        new.push(v);
+    }
+    debug!("generated new sub path: {:?} for original: {:?}", new, path);
+    Ok(new)
 }
 
 async fn do_check(paths: &[PathBuf], checker: &Checker) {
@@ -235,73 +336,44 @@ fn init_log(verbose: u8) -> Result<()> {
 }
 
 impl Common {
-    /// 从self.paths中读取md文件。
-    ///
-    /// todo: concurrent reading
+    /// 从self.paths中读取md文件paths。
     fn load_input_paths(&self) -> Result<Vec<PathBuf>> {
-        info!("loading all paths from {:?}", self.paths);
-        if self.paths.is_empty() {
-            return Err(anyhow!("empty input paths"));
-        }
-
-        let mut distinct = HashSet::new();
-        let mut paths = vec![];
-        for input in &self.paths {
-            for path in self.load_md_paths(&input)? {
-                let ca_path = path.canonicalize()?;
-                let s = ca_path
-                    .to_str()
-                    .ok_or_else(|| anyhow!("to str error"))
-                    .map(ToString::to_string)?;
-                if !distinct.insert(ca_path) {
-                    info!("duplicate path {} in top path: {:?}", s, input);
-                    continue;
-                }
-                paths.push(path);
-            }
-        }
+        let path = self.input.as_path().canonicalize()?;
+        let path_str = path
+            .to_str()
+            .ok_or_else(|| anyhow!("{:?} to str error", path))?;
         info!(
-            "loaded {} markdown files in input paths: {:?}",
-            paths.len(),
-            self.paths
+            "loading paths of markdown file from {} with extensions: {:?}",
+            path_str, self.extensions
         );
-        Ok(paths)
-    }
-
-    fn load_md_paths(&self, path: impl AsRef<Path>) -> Result<Vec<PathBuf>> {
-        let path = path.as_ref();
-        log::trace!("loading from {:?}", path);
         if !path.exists() {
-            return Err(anyhow!("{:?} does not exist", path.to_str()));
-        }
-
-        if self.recursive {
-            if !path.is_dir() {
-                return Err(anyhow!("{:?} is not dir", path.to_str()));
-            }
-            let mut paths = vec![];
-            return self.visite_recursive(path, &mut paths).map(|_| paths);
+            bail!("{} does not exist", path_str);
         }
 
         if path.is_file() {
-            if self.is_markdown_file(path) {
+            if self.recursive {
+                bail!("recursive flag [-r] is not available on file: {}", path_str);
+            }
+            if self.is_markdown_file(&path) {
                 return Ok(vec![path.to_path_buf()]);
             } else {
-                return Err(anyhow!(
-                    "{:?} is not markdown file: {:?}",
-                    path,
-                    self.extensions
-                ));
+                bail!("{} is not markdown file", path_str,);
             }
         }
 
-        Ok(path
-            .read_dir()?
-            .filter(Result::is_ok)
-            .map(Result::unwrap)
-            .map(|dir| dir.path())
-            .filter(|p| self.is_markdown_file(p))
-            .collect())
+        // dir
+        if self.recursive {
+            let mut paths = vec![];
+            self.visite_recursive(path, &mut paths).map(|_| paths)
+        } else {
+            Ok(path
+                .read_dir()?
+                .filter(Result::is_ok)
+                .map(|dir| dir.unwrap().path())
+                .filter(|p| self.is_markdown_file(p))
+                .map(|p| p.canonicalize().unwrap())
+                .collect())
+        }
     }
 
     fn is_markdown_file(&self, path: impl AsRef<Path>) -> bool {
@@ -324,11 +396,25 @@ impl Common {
                 self.visite_recursive(path, paths)?;
             } else if self.is_markdown_file(&path) {
                 log::trace!("found {:?}", path);
-                paths.push(path.to_path_buf());
+                paths.push(path.canonicalize()?);
             } else {
                 log::trace!("ignore un md file: {:?}", path);
             }
         }
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_output() -> Result<()> {
+        let input = "/root/a/b";
+        let path = "test/a.md";
+        let res = get_sub_path(input, &format!("{}/{}", input, path))?;
+        assert_eq!(res.to_str().unwrap(), path);
         Ok(())
     }
 }
